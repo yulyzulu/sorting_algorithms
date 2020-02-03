19ea59a1c34e105faa9e9210b24d089bf2dfd1e5
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-02-03 19:39:55 +0000

    Selection

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+#include <stdlib.h>
+/**
+*
+*
+*
+*/
+void insertion_sort_list(listint_t **list)
+{
+
+
+
+}
---
+++ 2-selection_sort.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+*selection_sort - sorts an array of integers in ascending order- Selection sort
+*@array: array of numbers
+*@size: size of array
+*/
+void selection_sort(int *array, size_t size)
+{
+	int min = 0, aux = 0;
+	size_t i, j;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+			{
+				min = j;
+			}
+		}
+		aux = array[i];
+		array[i] = array[min];
+		array[min] = aux;
+	}
+}
