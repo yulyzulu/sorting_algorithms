3b424168fa2e3c4c61cc254e0f78d8291bee1172
yulyzulu <yulyzulu05@gmail.com>
2020-02-04 22:07:58 +0000

    Sort with Quicksort algorithm

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -7,48 +7,68 @@
 */
 void quick_sort(int *array, size_t size)
 {
-	q_s(array, 0, size - 1);
+	if (array == NULL || size < 2)
+		return;
+	quicksort(array, 0, size - 1, size);
 }
 
 /**
-*q_s - sorts an array
+*swap- swap two variables
+*@a: pointer
+*@b: pointer
+*@array: array
+*@size: size of the array
+*/
+void swap(int *a, int *b, int *array, size_t size)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+	if (*a != temp)
+		print_array(array, size);
+}
+
+/**
+*partition - sorts an array
 *@array: array to sort
 *@first: first position
 *@last: last position
+*@size: size of the array
+*Return: Integer
 */
-void q_s(int *array, int first, int last)
+int partition(int *array, int first, int last, size_t size)
 {
-	int i, j, piv, temp;
+	int piv, i, j/*, aux, aux2*/;
+
+	piv = array[last];
+	i = first;
+	for (j = first; j < last; j++)
+	{
+		if (array[j] < piv)
+		{
+			swap(&array[i], &array[j], array, size);
+			i++;
+		}
+	}
+	swap(&array[i], &array[last], array, size);
+	return (i);
+}
+/**
+*quicksort - sort array
+*@array: array of integers
+*@first: first position of the array
+*@last: last position of the array
+*@size: size of the array
+*/
+void quicksort(int *array, int first, int last, size_t size)
+{
+	int tem;
 
 	if (first < last)
 	{
-		piv = first;
-		i = first;
-		j = last;
-
-		while (i < j)
-		{
-			while (array[i] <= array[piv] && i < last)
-			{
-				i++;
-			}
-			while (array[j] > array[piv])
-			{
-				j--;
-			}
-			if (i < j)
-			{
-				temp = array[i];
-				array[i] = array[j];
-				array[j] = temp;
-			}
-		}
-		temp = array[piv];
-		array[piv] = array[j];
-		array[j] = temp;
-		/*print_array(array, first + last);*/
-		q_s(array, first, j - 1);
-		q_s(array, j + 1, last);
+		tem = partition(array, first, last, size);
+		quicksort(array, first, tem - 1, size);
+		quicksort(array, tem + 1, last, size);
 	}
-}
 
+}
--- sort.h
+++ sort.h
@@ -25,5 +25,7 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void q_s(int *array, int first, int last);
+int partition(int *array, int first, int last, size_t size);
+void quicksort(int *array, int first, int last, size_t size);
 
 #endif
